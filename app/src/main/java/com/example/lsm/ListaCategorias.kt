431ac3b01646b80b50e938576462a6e5dba5f6d1
Paola <paolaht12@gmail.com>
2023-10-05 17:44:33 -0600

    se agrego un nuevo activity para la seccion de categorias

---
+++ app/src/main/java/com/example/lsm/ListaCategorias.kt
@@ -0,0 +1,11 @@
+package com.example.lsm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListaCategorias : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_categorias)
+    }
+}
