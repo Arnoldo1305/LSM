603dab97ab35e050d11e40e17db1355f225331e4
Paola Hernandez <paolaht12@gmail.com>
2023-12-29 13:23:35 -0600

    Se agrego una nueva funcion en AbecedarioFragment.kt para traer los datos de la descripcion de cada palabra de acuerdo a la palabra que se seleccione en la lista, y ya se muestra en InfoPalabraFragment.kt

--- app/src/main/java/com/example/lsm/AbecedarioFragment.kt
+++ app/src/main/java/com/example/lsm/AbecedarioFragment.kt
@@ -1,13 +1,12 @@
 package com.example.lsm
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
 class AbecedarioFragment : Fragment() {
@@ -24,30 +23,51 @@ class AbecedarioFragment : Fragment() {
     ): View? {
         val root = inflater.inflate(R.layout.fragment_abecedario, container, false)
         val arrayAdapter:ArrayAdapter<*>
-        val palabras = traerDatos()
+        val palabras = traerDatosPalabras()
+        //val descripcion = traerDatosDescripcion()
         val lvDatos = root.findViewById<ListView>(R.id.lvDatos)
 
         arrayAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1,palabras)
         lvDatos.adapter = arrayAdapter
 
         lvDatos.setOnItemClickListener(){parent,view,position,id->
-            //findNavController().navigate(R.id.action_categoriasFragment_to_abecedarioFragment)
+            val palabra = parent.getItemAtPosition(position).toString()
+            val descripcion = traerDatosDescripcion(palabra)
+            // Crear un Bundle y pone los datos en él, los cuales seran enviados mediante el navController
+            val bundle = Bundle().apply {
+                putString("palabra", palabra)
+                putString("descripcion", descripcion)
+            }
+            findNavController().navigate(R.id.action_abecedarioFragment_to_infoPalabraFragment, bundle)
         }
         return root
     }
 
-    private fun traerDatos():List<String> {
+    //Funcion para traer los datos de nuestra BD
+    private fun traerDatosPalabras():List<String> {
         val datos = dbHelper.listarDatos()
-        val palabras: MutableList<String> = arrayListOf();
+        val palabras: MutableList<String> = arrayListOf()
         if (datos.isNotEmpty()) {
             for (dato in datos) {
                 palabras.add(dato.texto)
             }
             return palabras
         } else {
-            val resp: List<String> = listOf<String>() + "No hay datos disponibles en la base de datos."
-            return resp
+            return listOf("No hay datos disponibles en la base de datos.")
         }
     }
 
+    //Funcion para traer la descripcion de las palabras de nuestra BD
+    private fun traerDatosDescripcion(palabra: String): String {
+        val datos = dbHelper.listarDatos()
+        if (datos.isNotEmpty()) {
+            for (dato in datos) {
+                if (dato.texto == palabra) {
+                    return dato.descripcion
+                }
+            }
+            //return "Descripción no encontrada."
+        }
+        return "Descripción no encontrada."
+    }
 }
